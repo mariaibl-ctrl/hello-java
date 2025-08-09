94df7548492de7934fcb857d0b3f98fc91595fcb
mariaibl-ctrl <ibalblolo@gmail.com>
2025-08-09 06:14:59 +0200

    Update HelloWorldExercises.java

--- basic/c00_helloworld/HelloWorldExercises.java
+++ basic/c00_helloworld/HelloWorldExercises.java
@@ -10,15 +10,49 @@ public class HelloWorldExercises {
     public static void main(String[] args) {
 
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
-
+public class HelloWorld { 
+    
+    public static void main(String[] args){ 
+        System.out.println("Isabel"); 
+    }
+}
+        
         // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
-
+public class HelloWorld { 
+    
+    public static void main(String[] args){ 
+        System.out.println("Hola\nMundo"); 
+    }
+}
+   
         // 3. Añade un comentario sobre lo que hace cada línea del programa.
-
+public class HelloWorld { //Crea una clase
+    
+    public static void main(String[] args){ //Inicia el main
+        System.out.println("Hola\nMundo"); //Impirme lo que se le pasa entre " "
+    }
+}        
         // 4. Crea un comentario en varias líneas.
-
+public class HelloWorld { 
+    
+    public static void main(String[] args){ 
+        System.out.println("Hola\nMundo"); 
+        /*
+        Comentario
+        en varias
+        lineas
+        */
+    }
+}
         // 5. Imprime tu edad, tu color favorito y tu ciudad.
-
+public class HelloWorld { 
+    
+    public static void main(String[] args){ 
+        System.out.println("Edad:23\nColor favorito:Negro\nCiudad:Leon"); 
+       
+    }
+}
+  
         // 6. Explora los diferentes System.XXX.println(); más allá de "out".
 
         // 7. Utiliza varios println para imprimir una frase.
